900002d9951210c560d5895e0810e1d3eb2d8b40
tangmi <tangmi@tangmidemac-mini.local>
2016-06-15 14:30:10 +0800

    添加gif介绍

--- ADScrollView/SubScrollViewAndPageView.h
+++ ADScrollView/SubScrollViewAndPageView.h
@@ -8,18 +8,32 @@
 
 #import <UIKit/UIKit.h>
 @class SubScrollViewAndPageView;
+
 @protocol SubScrollViewAndPageViewDelegate
+
 @required
+
 -(void)clickPage:(SubScrollViewAndPageView*)view atIndex:(NSInteger)index;
+
 @optional
+
 @end
-@interface SubScrollViewAndPageView : UIView<UIScrollViewDelegate>
+
+@interface SubScrollViewAndPageView : UIView <UIScrollViewDelegate>
+
 @property (nonatomic,strong) NSTimer *subTimer;
 
 @property (nonatomic,strong) NSMutableArray *allImageView;
-@property (nonatomic,strong) id<SubScrollViewAndPageViewDelegate>delegate;
+
+@property (nonatomic,strong) id<SubScrollViewAndPageViewDelegate> delegate;
+
 @property (nonatomic,strong,readonly) UIScrollView *subScrollView;
+
 @property (nonatomic,strong,readonly) UIPageControl *subPageControl;
--(void)shouldAutoPlay:(BOOL)isPlay;
+
 @property (nonatomic,assign) BOOL autoPlay;
+
+-(void)shouldAutoPlay:(BOOL)isPlay;
+
+
 @end
